445a7ef43bb6a30c97ad3b4946637bdd2a8173f9
hamzadevelops26-spec <hamzadevelops26@gmail.com>
2026-06-09 18:00:40 +0100

    create simple auto load and create create product conroller

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+spl_autoload_register(function ($class) {
+    require __DIR__  . "/src/$class.php";
+});
+header("Content-Type: application/json", "charset = UTF-8");
 $parts = explode("/", $_SERVER['REQUEST_URI']);
 if ($parts[2] !== "products") {
     http_response_code(404);
     exit;
 }
 $id = $parts[3] ?? null;
-var_dump($id);
+$controller = new ProductController;
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
